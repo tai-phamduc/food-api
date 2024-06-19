5fedf87924bfd319bc08b5871e486f37dcc55778
Pham Duc Tai <phamductai102703@gmail.com>
2024-06-19 17:28:43 +0700

    first commit

---
+++ backend/data.js
@@ -0,0 +1,112 @@
+db.foods.insertMany([
+  {
+    name: "Pizza Margherita",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Pizza_Margherita_stu_spivack.jpg/1280px-Pizza_Margherita_stu_spivack.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Pizza_Margherita_stu_spivack.jpg/1280px-Pizza_Margherita_stu_spivack.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Pizza_Margherita_stu_spivack.jpg/1280px-Pizza_Margherita_stu_spivack.jpg"
+    ],
+    type: "$$",
+    rating: 4.5,
+    reviews: 120
+  },
+  {
+    name: "Sushi Platter",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Sushi_platter.jpg/1024px-Sushi_platter.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Sushi_platter.jpg/1024px-Sushi_platter.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Sushi_platter.jpg/1024px-Sushi_platter.jpg"
+    ],
+    type: "$$$",
+    rating: 4.8,
+    reviews: 200
+  },
+  {
+    name: "Burger Deluxe",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Wendy%27s_Double_Bacon_Deluxe_hamburger.jpg/1280px-Wendy%27s_Double_Bacon_Deluxe_hamburger.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Wendy%27s_Double_Bacon_Deluxe_hamburger.jpg/1280px-Wendy%27s_Double_Bacon_Deluxe_hamburger.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Wendy%27s_Double_Bacon_Deluxe_hamburger.jpg/1280px-Wendy%27s_Double_Bacon_Deluxe_hamburger.jpg"
+    ],
+    type: "$",
+    rating: 4.2,
+    reviews: 85
+  },
+  {
+    name: "Pasta Carbonara",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Espaguetis_carbonara.jpg/1280px-Espaguetis_carbonara.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Espaguetis_carbonara.jpg/1280px-Espaguetis_carbonara.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Espaguetis_carbonara.jpg/1280px-Espaguetis_carbonara.jpg"
+    ],
+    type: "$$",
+    rating: 4.7,
+    reviews: 150
+  },
+  {
+    name: "Grilled Salmon",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Sashimi_-_Maguro_Restaurant%2C_Bangna%2C_Bangkok_%2844856596864%29.jpg/1280px-Sashimi_-_Maguro_Restaurant%2C_Bangna%2C_Bangkok_%2844856596864%29.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Sashimi_-_Maguro_Restaurant%2C_Bangna%2C_Bangkok_%2844856596864%29.jpg/1280px-Sashimi_-_Maguro_Restaurant%2C_Bangna%2C_Bangkok_%2844856596864%29.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Sashimi_-_Maguro_Restaurant%2C_Bangna%2C_Bangkok_%2844856596864%29.jpg/1280px-Sashimi_-_Maguro_Restaurant%2C_Bangna%2C_Bangkok_%2844856596864%29.jpg"
+    ],
+    type: "$$$",
+    rating: 4.9,
+    reviews: 95
+  },
+  {
+    name: "Tacos Al Pastor",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/01_Tacos_al_Pastor.jpg/1280px-01_Tacos_al_Pastor.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/01_Tacos_al_Pastor.jpg/1280px-01_Tacos_al_Pastor.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/01_Tacos_al_Pastor.jpg/1280px-01_Tacos_al_Pastor.jpg"
+    ],
+    type: "$",
+    rating: 4.3,
+    reviews: 110
+  },
+  {
+    name: "Ramen Bowl",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Shoyu_ramen%2C_at_Kasukabe_Station_%282014.05.05%29_1.jpg/1280px-Shoyu_ramen%2C_at_Kasukabe_Station_%282014.05.05%29_1.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Shoyu_ramen%2C_at_Kasukabe_Station_%282014.05.05%29_1.jpg/1280px-Shoyu_ramen%2C_at_Kasukabe_Station_%282014.05.05%29_1.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Shoyu_ramen%2C_at_Kasukabe_Station_%282014.05.05%29_1.jpg/1280px-Shoyu_ramen%2C_at_Kasukabe_Station_%282014.05.05%29_1.jpg"
+    ],
+    type: "$$",
+    rating: 4.6,
+    reviews: 140
+  },
+  {
+    name: "Steak Dinner",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Beef_fillet_steak_with_mushrooms.jpg/1280px-Beef_fillet_steak_with_mushrooms.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Beef_fillet_steak_with_mushrooms.jpg/1280px-Beef_fillet_steak_with_mushrooms.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Beef_fillet_steak_with_mushrooms.jpg/1280px-Beef_fillet_steak_with_mushrooms.jpg"
+    ],
+    type: "$$$",
+    rating: 4.8,
+    reviews: 130
+  },
+  {
+    name: "Vegan Salad",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Salad_platter.jpg/1280px-Salad_platter.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Salad_platter.jpg/1280px-Salad_platter.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Salad_platter.jpg/1280px-Salad_platter.jpg"
+    ],
+    type: "$",
+    rating: 4.4,
+    reviews: 70
+  },
+  {
+    name: "Chocolate Cake",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Brown_Chocolate_Cake.jpg/1280px-Brown_Chocolate_Cake.jpg",
+    photos: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Brown_Chocolate_Cake.jpg/1280px-Brown_Chocolate_Cake.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Brown_Chocolate_Cake.jpg/1280px-Brown_Chocolate_Cake.jpg"
+    ],
+    type: "$$",
+    rating: 4.9,
+    reviews: 160
+  }
+])
---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const asyncHandler = require("express-async-handler")
+
+mongoose.connect("mongodb://127.0.0.1:27017/food-app")
+  .then(() => console.log("Successfully connect to mongod"))
+  .catch((err) => console.log("Cannot connect to mongod", err))
+
+const FoodSchema = new mongoose.Schema({
+  name: { type: String, required: true},
+  avatar: { type: String, required: true},
+  photos: { type: Array, required: true},
+  type: { type: String, required: true, enum: ["$", "$$", "$$$"]},
+  rating: { type: Number, required: true },
+  reviews: { type: Number, required: true}
+})
+const FoodModel = mongoose.model("Food", FoodSchema)
+
+const authHandler = ( req, res, next) => {
+  let token
+  if (req.headers.authorization) {
+    token = req.headers.authorization.toString().split(" ")[1]
+    if (token != "youtube") {
+      console.log("Youtube")
+      res.status(401)
+      throw new Error("Not authorized")
+    }
+  }
+  if (!token) {
+    res.status(401)
+    throw new Error("Not authorized")
+  }
+  next()
+}
+
+app.get("/api/foods", authHandler, asyncHandler(async (req, res) => {
+  const foods = await FoodModel.find({})
+  res.status(200).json(foods)
+}))
+
+const errorHandler = (err, req, res, next) => {
+  const status = res.statusCode ? res.statusCode : 500
+  res.status(status)
+  res.json({ message: err.message})
+}
+
+app.use(errorHandler)
+
+app.listen(5000, () => {
+  console.log("Port 5000 has been started")
+})
+
+export default app
